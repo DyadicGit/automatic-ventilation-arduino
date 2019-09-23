2e4f1305a50d4ad956953e84d246ca31c6ccc5e6
Dyadic <dyadicg@gmail.com>
2019-09-23 21:14:50 +0300

    new prod values again, in winter they've changed

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@
 //User variables:
 const int loopEvery = 2000;                                                         //<-- set time to read the value every (in miliseconds)
 const int gasValueCO2 = 120;                                                       // <-- set this when to clear the air
-const int gasValueCH4 = 210;                                                       // <-- set this when to clear the air
-const unsigned long int fanManualWorkTime = (unsigned long int)1000 * 60 * 120;    // set manual fan working time
-const unsigned long int fanTurboModeWorkTime = (unsigned long int)1000 * 60 * 60;  // set fan on turbo mode working time, should be less tan fanManualWorkTime!
+const int gasValueCH4 = 290;                                                       // <-- set this when to clear the air
+const unsigned long int fanManualWorkTime = (unsigned long int)1000 * 60 * 60;    // set manual fan working time
+const unsigned long int fanTurboModeWorkTime = (unsigned long int)1000 * 60 * 35;  // set fan on turbo mode working time, should be less tan fanManualWorkTime!
                                                                                    // ex.: fanManualWorkTime=60min & fanTurboModeWorkTime=45min, then the fan will work 15mins in turbo mode,the it'll switch to low speed
 const unsigned long int fanAutomaticWorkTime = (unsigned long int)1000 * 60 * 30; // set automatic fan working time
 const unsigned long debounceIgnore = 150;                                          // If button press come faster than 50ms ("this variable"), assume it's a bounce and ignore
